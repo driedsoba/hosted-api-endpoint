269f29540853492d6bdc24a344315ca1e7cae44f
driedsoba <junleliw@gmail.com>
2026-04-11 21:01:28 +0800

    fix: remove reserved concurrency, account limit too low

--- terraform/modules/compute/main.tf
+++ terraform/modules/compute/main.tf
@@ -8,16 +8,16 @@ data "archive_file" "lambda_package" {
 #checkov:skip=CKV_AWS_116:Lambda is invoked synchronously via API Gateway - DLQ does not apply
 #checkov:skip=CKV_AWS_117:Lambda does not access VPC resources - VPC placement unnecessary
 #checkov:skip=CKV_AWS_50:X-Ray tracing incurs cost beyond free tier - out of scope
+#checkov:skip=CKV_AWS_115:Account concurrency limit too low to reserve executions
 resource "aws_lambda_function" "api" {
-  function_name                  = var.function_name
-  role                           = aws_iam_role.lambda_execution.arn
-  handler                        = "lambda_handler.handler"
-  runtime                        = "python3.12"
-  memory_size                    = var.lambda_memory_size
-  timeout                        = var.lambda_timeout
-  filename                       = data.archive_file.lambda_package.output_path
-  source_code_hash               = data.archive_file.lambda_package.output_base64sha256
-  reserved_concurrent_executions = 10
+  function_name    = var.function_name
+  role             = aws_iam_role.lambda_execution.arn
+  handler          = "lambda_handler.handler"
+  runtime          = "python3.12"
+  memory_size      = var.lambda_memory_size
+  timeout          = var.lambda_timeout
+  filename         = data.archive_file.lambda_package.output_path
+  source_code_hash = data.archive_file.lambda_package.output_base64sha256
 
   environment {
     variables = {
